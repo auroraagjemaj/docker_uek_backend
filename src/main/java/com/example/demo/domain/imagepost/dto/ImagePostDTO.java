40a8d54aa0ed5c68b89ea7718b04fda517a95595
Nikola Bogosavljevic <nikola.bogosavljevic@noseryoung.com>
2023-09-08 16:09:31 +0200

    finsihed documentation pdf

---
+++ src/main/java/com/example/demo/domain/imagepost/dto/ImagePostDTO.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.imagepost.dto;
+
+import com.example.demo.core.generic.AbstractDTO;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@Data
+
+public class ImagePostDTO extends AbstractDTO {
+}
