5433299bcfa178d63010af63ffb8c218a31669d5
Nikola Bogosavljevic <nikola.bogosavljevic@noseryoung.com>
2023-09-11 13:06:40 +0200

    finished dto

--- src/main/java/com/example/demo/domain/imagepost/ImagePostController.java
+++ src/main/java/com/example/demo/domain/imagepost/ImagePostController.java
@@ -1,6 +1,8 @@
 package com.example.demo.domain.imagepost;
 
 
+import com.example.demo.domain.imagepost.dto.ImagePostDTO;
+import com.example.demo.domain.imagepost.dto.ImagePostMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +17,16 @@ public class ImagePostController {
 
     @Autowired
     private ImagePostServiceImpl imagePostService;
+    @Autowired
+    private ImagePostMapper imagePostMapper;
 
-    @GetMapping("/getAll")
-    public ResponseEntity<List<ImagePost>> retrieveAll(){
-        return new ResponseEntity<>(imagePostService.findAll(), HttpStatus.OK);
+    @GetMapping({"", "/"})
+    public ResponseEntity<List<ImagePostDTO>> retrieveAll(){
+        List<ImagePost> imagePosts = imagePostService.findAll();
+        return new ResponseEntity<>(imagePostMapper.toDTOs(imagePosts), HttpStatus.OK);
     }
 
-    @PostMapping("/create")
+    @PostMapping({"", "/"})
     public ResponseEntity createImagePost(@RequestBody ImagePost imagePost){
         imagePostService.save(imagePost);
         return ResponseEntity.ok().body("Created post!");
