360ee8753bb5c4d4e7f19334e6bcb0734d17dd6c
Nikola Bogosavljevic <nikola.bogosavljevic@noseryoung.com>
2023-09-13 09:26:22 +0200

    added authorization and update method

--- src/main/java/com/example/demo/domain/imagepost/ImagePostController.java
+++ src/main/java/com/example/demo/domain/imagepost/ImagePostController.java
@@ -3,9 +3,11 @@ package com.example.demo.domain.imagepost;
 
 import com.example.demo.domain.imagepost.dto.ImagePostDTO;
 import com.example.demo.domain.imagepost.dto.ImagePostMapper;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,15 +28,24 @@ public class ImagePostController {
         return new ResponseEntity<>(imagePostMapper.toDTOs(imagePosts), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('POST_CREATE')")
     @PostMapping({"", "/"})
-    public ResponseEntity createImagePost(@RequestBody ImagePost imagePost){
+    public ResponseEntity createImagePost(@Valid @RequestBody ImagePost imagePost){
         imagePostService.save(imagePost);
         return ResponseEntity.ok().body("Created post!");
     }
 
+    @PreAuthorize("hasAuthority('POST_DELETE')")
     @DeleteMapping("/{id}")
-    public ResponseEntity deletePublisher(@PathVariable ("id") UUID id){
+    public ResponseEntity deleteImagePost(@PathVariable ("id") UUID id){
         imagePostService.deleteById(id);
         return ResponseEntity.ok().body("Deleted publisher");
+    }
+
+    @PreAuthorize("hasAuthority('POST_UPDATE')")
+    @PutMapping("/{id}")
+    public ResponseEntity updateImagePost(@Valid @RequestBody ImagePostDTO imagePostDTO, @PathVariable ("id") UUID id){
+        imagePostService.updateById(id, imagePostMapper.fromDTO(imagePostDTO));
+        return ResponseEntity.ok().body("Updated post!");
     }
 }
