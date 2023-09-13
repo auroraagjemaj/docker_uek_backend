e77656df68c1c39efd35386202d02f2ce75f59b2
Nikola Bogosavljevic <nikola.bogosavljevic@noseryoung.com>
2023-09-13 11:57:48 +0200

    added get all test in postman

--- src/main/java/com/example/demo/domain/imagepost/ImagePostServiceImpl.java
+++ src/main/java/com/example/demo/domain/imagepost/ImagePostServiceImpl.java
@@ -1,13 +1,16 @@
 package com.example.demo.domain.imagepost;
 
-import com.example.demo.core.generic.AbstractRepository;
 import com.example.demo.core.generic.AbstractServiceImpl;
-import com.example.demo.domain.user.User;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class ImagePostServiceImpl extends AbstractServiceImpl<ImagePost> {
 
+    @Autowired
     public ImagePostServiceImpl(ImagePostRepository repository) {
         super(repository);
     }
